0e19c667c387e70274603570a139cd2c04e203a6
luiseufrasio <luis.eufrasio@gmail.com>
2021-09-03 21:57:39 -0300

    Template Context

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,4 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-	return render(request, 'home_page.html', {})
+	context = {
+		"title": "Hello World",
+		"content": "Welcome my friend!"
+	}
+	return render(request, 'home_page.html', context)
+
+def about_page(request):
+	context = {
+		"title": "About",
+		"content": "Welcome to my description!"
+	}
+	return render(request, 'home_page.html', context)
+
+def contact_page(request):
+	context = {
+		"title": "Contact",
+		"content": "Welcome to my contacts!"
+	}
+	return render(request, 'home_page.html', context)
