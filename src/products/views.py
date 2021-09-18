d719cf07139bbe828916a08fadb7606e2ffa71eb
luiseufrasio <luis.eufrasio@gmail.com>
2021-09-18 10:17:57 -0300

    DetailView

--- src/products/views.py
+++ src/products/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Product
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
+    model = Product
     template_name = "products/list.html"
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product.html"
