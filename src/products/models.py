4b8705ab31a3e2f85290793aca8027514150cf17
luiseufrasio <luis.eufrasio@gmail.com>
2021-09-18 18:48:27 -0300

    Custom Model Manager

--- src/products/models.py
+++ src/products/models.py
@@ -11,11 +11,20 @@ def upload_image_path(instance, file_name):
 	return f'products/{random_name}/{random_name}{ext}'
 
 # Create your models here.
+class ProductManager(models.Manager):
+	def get_by_id(self, id):
+		qs = self.get_queryset().filter(id=id)
+		if qs.count() == 1:
+			return qs.first()
+		return None
+
 class Product(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextField()
 	price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
 	image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+
+	objects = ProductManager()
 
 	def __str__(self):
 		return self.title
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-
+from django.http import Http404
 from .models import Product
 
 class ProductListView(ListView):
@@ -10,3 +10,10 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = "products/product.html"
+
+    def get_object(self, *args, **kwargs):
+    	pk = self.kwargs.get('pk')
+    	object = Product.objects.get_by_id(pk)
+    	if object is None:
+    		raise Http404("Product doesn't exist")
+    	return object
