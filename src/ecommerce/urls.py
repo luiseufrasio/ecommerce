5a22a5198a60cfa9a03179788ba1ada045631441
luiseufrasio <luis.eufrasio@gmail.com>
2021-09-21 16:39:11 -0300

    Product URLs

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -17,15 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
-
-from products.views import (
-    ProductListView, 
-    ProductDetailView,
-    ProductDetailSlugView,
-    ProductFeaturedListView, 
-    ProductFeaturedDetailView
-    )
+from django.urls import path, include
 
 from .views import home_page, contact_page, about_page, login_page, register_page
 
@@ -35,11 +27,7 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
-    path('products/', ProductListView.as_view()),
-    path('products/<int:pk>/', ProductDetailView.as_view()),
-    path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
-    path('featured/', ProductFeaturedListView.as_view()),
-    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
+    path('products/', include("products.urls")),
     path('admin/', admin.site.urls),
 ]
 
